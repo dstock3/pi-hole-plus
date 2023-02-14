58aebf49b103a212e235392b41d7a7e621b76a1c
David Stockdale <dstock3@gmail.com>
2023-02-14 13:09:27 -0600

    revised error functions.

--- ext/popup.js
+++ ext/popup.js
@@ -15,11 +15,13 @@ const errorContainer = document.querySelector('.error-container');
 
 const displayError = (error) => {
   errorContainer.textContent = error;
+  errorContainer.style.display = 'block';
   errorContainer.classList.remove('hide');
 };
 
 const removeError = () => {
   errorContainer.textContent = '';
+  errorContainer.style.display = 'none';
   errorContainer.classList.add('hide');
 };
 
