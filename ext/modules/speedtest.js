3fdd78951944eeeadb76676df1ad634c17c93c21
David Stockdale <dstock3@gmail.com>
2023-02-18 11:30:13 -0600

    Revised the speedtest module to assign colors to the rating element based on test results.

--- ext/modules/speedtest.js
+++ ext/modules/speedtest.js
@@ -9,16 +9,34 @@ const runSpeedTest = async () => {
 };
 
 export const updateSpeedTest = async () => {
-    const results = await runSpeedTest();
-    const downloadSpeed = document.querySelector('#download-speed');
-    const rating = document.querySelector('#rating');
-  
-    if (results) {
-      downloadSpeed.textContent = `Download Speed: ${results.speed}`;
-      rating.textContent = `Rating: ${results.rating}`;
-    } else {
-      downloadSpeed.textContent = 'Could not perform speed test';
-      rating.textContent = '';
+  const results = await runSpeedTest();
+  const downloadSpeed = document.querySelector('#download-speed');
+  const rating = document.querySelector('#rating');
+
+  if (results && results.speed && results.rating) {
+    downloadSpeed.textContent = `Download Speed: ${results.speed}`;
+    rating.textContent = `Rating: ${results.rating}`;
+    
+    switch (results.rating) {
+      case 'Excellent':
+        rating.style.color = '#00c853';
+        break;
+      case 'Very good':
+        rating.style.color = '#43a047';
+        break;
+      case 'Good':
+        rating.style.color = '#7cb342';
+        break;
+      case 'Fair':
+        rating.style.color = '#ffff00';
+        break;
+      case 'Poor':
+        rating.style.color = '#ff0000';
+        break;
     }
+  } else {
+    downloadSpeed.textContent = 'Could not perform speed test';
+    rating.textContent = '';
+  }
 };
-  
+
